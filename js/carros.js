35b05fef9c00f2f167db584f900b9082d40dd56d
Daniel <daniel.rodriguez.finol@gmail.com>
2020-04-13 12:24:16 -0500

    Terminado

---
+++ js/carros.js
@@ -0,0 +1,59 @@
+class vehiculo {
+
+    cantidadDePuertas = ''
+    año = ''
+    marcaDeFabricante = ''
+    inventor = ''
+    velocidadMaxima = ''
+    aceleracion = ''
+
+    constructor(cantidadDePuertas,aceleracion,velocidadMaxima,año,marcaDeFabricante,inventor) {
+        this.cantidadDePuertas = cantidadDePuertas
+        this.año = año
+        this.marcaDeFabricante = marcaDeFabricante
+        this.inventor = inventor
+        this.velocidadMaxima = velocidadMaxima
+        this.aceleracion = aceleracion
+    }
+
+    arrancar() {
+        console.log(`El ${this.nombre} acaba de empezar su recorrido, con una aceleracion de 0kph - 100kph en tan solo ${this.aceleracion}s, un novedoso diseño de ${this.cantidadDePuertas} puertas. Fabricado en ${this.año} por ${this.marcaDeFabricante}. Fue diseñado por ${this.inventor}`)
+    }
+
+    despegue() {
+        console.log(`El avion ${this.nombre} ha despegado, fue fabricado en ${this.año} por ${this.marcaDeFabricante} y diseñado por ${this.inventor}. Esta hermosa nave consigue una velocidad maxima de ${this.velocidadMaxima} a pesar de su peso de ${this.peso}`)
+    } 
+
+    navegar() {
+        console.log(`Ya estoy ladillado, esto se llama ${this.nombre} es una lancha de alta velocidad que llega a ${this.velocidadMaxima} fabricada en ${this.año} por ${this.marcaDeFabricante}`)
+    }
+}
+
+
+class porsche extends vehiculo{
+    constructor(nombre) {
+        super(2,'3,2' ,312, 2019,'Porsche AG', 'Ferdinand Porsche')
+        this.nombre = nombre
+    }
+}
+
+class Avion extends vehiculo {
+    constructor(nombre,cantidadDeMotores,peso) {
+        super('','','2.700kph',1997,'Sukhoi Corporation.','Tsibin LL')
+        // super(,,  ,' km/h')
+        this.nombre = nombre
+        this.cantidadDeMotores = cantidadDeMotores
+        this.peso = peso
+    }         
+}
+
+class lancha extends vehiculo {
+    constructor(nombre) {
+        super('','aceleracion',' 217 km/h ',2007,'AMG','')
+        this.nombre = nombre
+    }
+}
+
+const Porsche = new porsche('porsche').arrancar()
+const Sukhoi = new Avion('Sukhoi Su-47',2, '16.380kg').despegue()
+const Marauder_AMG = new lancha(`50' Marauder AMG`).navegar()
---
+++ js/script.js
@@ -0,0 +1,78 @@
+class Animal {
+
+    nombreAnimal = ''
+    numeroDePatas = ''
+    especie = ''
+    nombre = ''
+
+
+    constructor(nombreAnimal,numeroDePatas,especie) {
+        this.nombreAnimal = nombreAnimal
+        this.numeroDePatas = numeroDePatas
+        this.especie = especie
+    }
+
+    saludar() {
+        console.log(`Hola, soy ${this.nombre}, un ${this.nombreAnimal} tengo ${this.numeroDePatas} patas, soy un ${this.especie}`)
+    }
+}
+
+class Ave extends Animal {
+    constructor(nombre) {
+        super('Ave', 2 ,'ovipero')
+        this.nombre = nombre;
+    }
+}
+
+class Perro extends Animal {
+    constructor(nombre) {
+        super('Perro', 4, 'canino');
+        this.nombre = nombre;
+    }
+}
+
+const fifi = new Perro('Fifi');
+fifi.saludar();
+
+let animal = new Ave('Carlos')    
+animal.saludar();
+
+// class persona  {
+//     constructor(edad,nombre) {
+//         this.edad = edad
+//         this.nombre = nombre
+//     }
+    
+//     saludar () {
+//         console.log(`Hola, soy ${this.nombre} y tengo ${this.edad}`)
+//     }
+
+//     despedir() {
+//         console.log('Adios')
+//     }
+// }
+
+
+// class PersonaImportante  extends persona{
+
+//     constructor(edad,nombre,profesion) {
+//         super(edad,nombre)
+//         this.profesion = profesion
+//     }
+
+
+//     saludarPorProfesion() {
+//         // this.saludar()
+//         console.log(`Soy,${this.nombre} tengo ${this.edad} y trabajo de ${this.profesion}`)
+//     }
+
+
+//     Despedida() {
+//         setTimeout(this.despedir,1000)
+//     }
+// }
+
+// let personaImportante = new PersonaImportante(18, 'Daniel','developerJr')
+
+// personaImportante.saludarPorProfesion()
+// personaImportante.Despedida()
