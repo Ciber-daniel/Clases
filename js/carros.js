ecee4292df1e413c1a8beb12f2f77042965fff69
Daniel <daniel.rodriguez.finol@gmail.com>
2021-01-31 11:12:37 -0500

    Fix indentation and some details

--- js/carros.js
+++ js/carros.js
@@ -1,62 +1,74 @@
 class Vehiculo {
+  cantidadDePuertas = 0;
+  año = 0;
+  marcaDeFabricante = '';
+  inventor = '';
+  velocidadMaxima = 0;
+  aceleracion = 0;
 
-    cantidadDePuertas = 0
-    año = 0
-    marcaDeFabricante = ''
-    inventor = ''
-    velocidadMaxima = 0
-    aceleracion = 0
+  constructor(
+    nombre,
+    cantidadDePuertas,
+    aceleracion,
+    velocidadMaxima,
+    año,
+    marcaDeFabricante,
+    inventor
+  ) {
+    this.nombre = nombre;
+    this.cantidadDePuertas = cantidadDePuertas;
+    this.año = año;
+    this.marcaDeFabricante = marcaDeFabricante;
+    this.inventor = inventor;
+    this.velocidadMaxima = velocidadMaxima;
+    this.aceleracion = aceleracion;
+  }
 
-    constructor(nombre, cantidadDePuertas, aceleracion, velocidadMaxima, año, marcaDeFabricante, inventor) {
-        this.nombre = nombre
-        this.cantidadDePuertas = cantidadDePuertas
-        this.año = año
-        this.marcaDeFabricante = marcaDeFabricante
-        this.inventor = inventor
-        this.velocidadMaxima = velocidadMaxima
-        this.aceleracion = aceleracion
-    }
+  arrancar() {
+    console.log(
+      `El ${this.nombre} acaba de empezar su recorrido, con una aceleracion de 0kph - 100kph en tan solo ${this.aceleracion}s, un novedoso diseño de ${this.cantidadDePuertas} puertas. Fabricado en ${this.año} por ${this.marcaDeFabricante}. Fue diseñado por ${this.inventor}`
+    );
+  }
 
-    arrancar() {
-        console.log(`El ${this.nombre} acaba de empezar su recorrido, con una aceleracion de 0kph - 100kph en tan solo ${this.aceleracion}s, un novedoso diseño de ${this.cantidadDePuertas} puertas. Fabricado en ${this.año} por ${this.marcaDeFabricante}. Fue diseñado por ${this.inventor}`)
-    }
+  despegar() {
+    console.log(
+      `El avion ${this.nombre} ha despegado, fue fabricado en ${this.año} por ${this.marcaDeFabricante} y diseñado por ${this.inventor}. Esta hermosa nave consigue una velocidad maxima de ${this.velocidadMaxima}kph a pesar de su peso de ${this.peso}kg`
+    );
+  }
 
-    despegar() {
-        console.log(`El avion ${this.nombre} ha despegado, fue fabricado en ${this.año} por ${this.marcaDeFabricante} y diseñado por ${this.inventor}. Esta hermosa nave consigue una velocidad maxima de ${this.velocidadMaxima}kph a pesar de su peso de ${this.peso}kg`)
-    } 
-
-    navegar() {
-        console.log(`Ya estoy ladillado, esto se llama ${this.nombre} es una lancha de alta velocidad que llega a ${this.velocidadMaxima}Kph fabricada en ${this.año} por ${this.marcaDeFabricante}`)
-    }
+  navegar() {
+    console.log(
+      `Ya estoy ladillado, esto se llama ${this.nombre} es una lancha de alta velocidad que llega a ${this.velocidadMaxima}Kph fabricada en ${this.año} por ${this.marcaDeFabricante}`
+    );
+  }
 }
 
-
-class Porsche extends Vehiculo{
-    constructor(nombre) {
-        super(2, 3.2 , 312, 2019, 'Porsche AG', 'Ferdinand Porsche')
-        this.nombre = nombre
-    }
+class Porsche extends Vehiculo {
+  constructor(nombre) {
+    super(2, 3.2, 312, 2019, 'Porsche AG', 'Ferdinand Porsche');
+    this.nombre = nombre;
+  }
 }
 
 class AvionDeCombate extends Vehiculo {
-    constructor(nombre, cantidadDeMotores, peso) {
-        super(0, 0, 2700, 1997, 'Sukhoi Corporation.', 'Tsibin LL')
-        this.nombre = nombre
-        this.cantidadDeMotores = cantidadDeMotores
-        this.peso = peso
-    }         
+  constructor(nombre, cantidadDeMotores, peso) {
+    super(0, 0, 2700, 1997, 'Sukhoi Corporation.', 'Tsibin LL');
+    this.nombre = nombre;
+    this.cantidadDeMotores = cantidadDeMotores;
+    this.peso = peso;
+  }
 }
 
 class Lancha extends Vehiculo {
-    constructor(nombre) {
-        super(0, 0, 217 ,2007,'AMG','')
-        this.nombre = nombre
-    }
+  constructor(nombre) {
+    super(0, 0, 217, 2007, 'AMG', '');
+    this.nombre = nombre;
+  }
 }
 
-const porsche = new Porsche('porsche')
-porsche.arrancar()
-const sukhoi = new AvionDeCombate('Sukhoi Su-47',2, 16380)
-sukhoi.despegar()
-const sarauderAmg = new Lancha(`50' Marauder AMG`)
-sarauderAmg.navegar()
+function setup() {
+  new Porsche('porsche').arrancar();
+  new AvionDeCombate('Sukhoi Su-47', 2, 16380).despegar();
+  new Lancha(`50' Marauder AMG`).navegar();
+}
+setup();
--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 class Animal {
+  nombreAnimal = '';
+  numeroDePatas = 0;
+  especie = '';
+  nombre = '';
 
-    nombreAnimal = ''
-    numeroDePatas = 0
-    especie = ''
-    nombre = ''
+  constructor(nombreAnimal, numeroDePatas, especie) {
+    this.nombreAnimal = nombreAnimal;
+    this.numeroDePatas = numeroDePatas;
+    this.especie = especie;
+  }
 
-
-    constructor(nombreAnimal,numeroDePatas,especie) {
-        this.nombreAnimal = nombreAnimal
-        this.numeroDePatas = numeroDePatas
-        this.especie = especie
-    }
-
-    saludar() {
-        console.log(`Hola, soy ${this.nombre}, un ${this.nombreAnimal} tengo ${this.numeroDePatas} patas, soy un ${this.especie}`)
-    }
+  saludar() {
+    console.log(
+      `Hola, soy ${this.nombre}, un ${this.nombreAnimal} tengo ${this.numeroDePatas} patas, soy un ${this.especie}`
+    );
+  }
 }
 
 class Ave extends Animal {
-    constructor(nombre) {
-        super('Ave', 2 ,'ovipero')
-        this.nombre = nombre;
-    }
+  constructor(nombre) {
+    super('Ave', 2, 'ovipero');
+    this.nombre = nombre;
+  }
 }
 
 class Perro extends Animal {
-    constructor(nombre) {
-        super('Perro', 4, 'canino');
-        this.nombre = nombre;
-    }
+  constructor(nombre) {
+    super('Perro', 4, 'canino');
+    this.nombre = nombre;
+  }
 }
 
-const fifi = new Perro('Fifi');
-fifi.saludar();
-
-let animal = new Ave('Carlos')    
-animal.saludar();
+function setup() {
+  const fifi = new Perro('Fifi').saludar();
+  let animal = new Ave('Carlos').saludar();
+}
+setup();
